ad9b1dfa8eed95f6c0e896081d433f87461c1d91
Nycole Becker Rodrigues <82180015+NycoleBecker@users.noreply.github.com>
2022-06-30 20:56:58 -0300

    updates Repository

---
+++ atividadeAS/models/repository/LivroRepository.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using atividadeAS.models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace atividadeAS.models.repository
+{
+    public class LivroRepository : ILivroRepository
+    {
+        private Datacontext context;
+
+        public LivroRepository(Datacontext context){
+            this.context = context;
+        }
+
+        public void Create(Livro entity)
+        {
+            context.Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var del = GetByIdAsync(id);
+            context.Remove(del);
+        }
+
+        public async Task <List<Livro>> GetAll()
+        {
+            return await context.DbSetbook.ToListAsync();
+        }
+
+        public void Update(Livro book)
+        {
+            context.Entry(book).State = EntityState.Modified;
+        }
+
+        public async Task<Livro> GetByIdAsync(int id)
+        {
+            return await context.DbSetbook.SingleOrDefaultAsync(x => x.Id_Livro == id);
+        }
+    }
+
+    public interface ILivroRepository
+    {
+    }
+}
